1d54481077af5c0b1611091b96da53923fc517b6
Ilya Roshupkin <ilyalive3@yandex.ru>
2025-03-22 20:07:34 +0300

    project

---
+++ src/app.js
@@ -0,0 +1,81 @@
+import styles from './app.module.css';
+import data from './data.json';
+import { useState } from 'react';
+
+export const App = () => {
+	// Можно задать 2 состояния — steps и activeIndex
+	const [steps] = useState(data);
+	const [activeIndex, setActiveIndex] = useState(0);
+
+	// И определить 3 обработчика: Клик назад, Клик вперед, Начать сначала
+	const pushBack = () => {
+		if (activeIndex > 0) {
+			setActiveIndex((item) => item - 1);
+		}
+	};
+
+	const pushForward = () => {
+		setActiveIndex((item) => item + 1);
+	};
+
+	const startOver = () => {
+		setActiveIndex(0);
+	};
+
+	// И 2 переменных-флага — находимся ли мы на первом шаге, и находимся ли на последнем
+	const firstStep = activeIndex === 0;
+	const lastSteps = activeIndex === steps.length - 1;
+
+	console.log(data);
+
+	return (
+		<div className={styles.container}>
+			<div className={styles.card}>
+				<h1>Инструкция по готовке пельменей</h1>
+				<div className={styles.steps}>
+					<div className={styles['steps-content']}>
+						{/* Для получения активного контента использйте steps и activeIndex */}
+						{steps[activeIndex].content}
+					</div>
+					<ul className={styles['steps-list']}>
+						{/* Выводите <li> с помощью массива steps и метода map(), подставляя в разметку нужные значения и классы */}
+						{steps.map(({ title, id }, index) => (
+							<li
+								key={id}
+								className={
+									styles['steps-item'] +
+									(index === activeIndex ? ` ${styles.active}` : '') +
+									(index < activeIndex ? ` ${styles.done}` : '')
+								}
+							>
+								<button
+									className={styles['steps-item-button']}
+									onClick={() => setActiveIndex(index)}
+								>
+									{index + 1}
+								</button>{' '}
+								{title}
+							</li>
+						))}
+					</ul>
+
+					<div className={styles['buttons-container']}>
+						<button
+							className={styles.button}
+							onClick={pushBack}
+							disabled={firstStep}
+						>
+							Назад
+						</button>
+						<button
+							className={styles.button}
+							onClick={() => (lastSteps ? startOver() : pushForward())}
+						>
+							{lastSteps ? 'Начать сначала' : 'Далее'}
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
